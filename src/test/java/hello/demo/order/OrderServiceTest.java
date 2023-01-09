dab180a4b66c37aeaed5d75d5677361716a1911e
LDY <leedy3838@naver.com>
2023-01-09 14:53:18 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ src/test/java/hello/demo/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package hello.demo.order;
+
+import hello.demo.member.Grade;
+import hello.demo.member.Member;
+import hello.demo.member.MemberService;
+import hello.demo.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder(){
+        //given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        //then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
