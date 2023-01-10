39c02d464717a126efeffd0ea49eee51b1f7897b
LDY <leedy3838@naver.com>
2023-01-10 13:25:02 +0900

    관심사의 분리

--- src/main/java/hello/demo/MemberApp.java
+++ src/main/java/hello/demo/MemberApp.java
@@ -3,16 +3,16 @@ package hello.demo;
 import hello.demo.member.Grade;
 import hello.demo.member.Member;
 import hello.demo.member.MemberService;
-import hello.demo.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args){
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
         System.out.println("new Member = " + member.getName());
-        System.out.println("fing Member = " + findMember.getName());
+        System.out.println("find Member = " + findMember.getName());
     }
 }
