5d22ad094826ee650c8b4146c8ec15ee4267b8dc
LDY <leedy3838@naver.com>
2023-01-10 13:53:22 +0900

    AppConfig 리팩터링

--- src/main/java/hello/demo/AppConfig.java
+++ src/main/java/hello/demo/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.demo;
 
+import hello.demo.discount.DiscountPolicy;
 import hello.demo.discount.FixDiscountPolicy;
+import hello.demo.member.MemberRepository;
 import hello.demo.member.MemberService;
 import hello.demo.member.MemberServiceImpl;
 import hello.demo.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import hello.demo.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
