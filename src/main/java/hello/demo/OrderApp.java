201786db1ca6195ca03cb2cd08767df9d99cc174
LDY <leedy3838@naver.com>
2023-01-10 19:24:37 +0900

    스프링 컨테이너를 이용한 의존관계 주입

--- src/main/java/hello/demo/OrderApp.java
+++ src/main/java/hello/demo/OrderApp.java
@@ -5,12 +5,15 @@ import hello.demo.member.Member;
 import hello.demo.member.MemberService;
 import hello.demo.order.Order;
 import hello.demo.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args){
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
