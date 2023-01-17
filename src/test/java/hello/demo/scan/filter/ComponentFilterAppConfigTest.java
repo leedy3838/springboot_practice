72b5708cd48cd98ca3976aac5c30891199e81c5d
LDY <leedy3838@naver.com>
2023-01-17 14:08:31 +0900

    필터

---
+++ src/test/java/hello/demo/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,34 @@
+package hello.demo.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA bean = ac.getBean(BeanA.class);
+        Assertions.assertThat(bean).isNotNull();
+
+        assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class));
+    }
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    )
+    static class ComponentFilterAppConfig{
+
+    }
+}
