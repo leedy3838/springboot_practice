ecdb598508e85a7b6c8f2d73c211bb05d5a4bbd3
LDY <leedy3838@naver.com>
2023-08-03 14:15:29 +0900

    Form 전송 객체 분리 - 개발

--- springboot_practice_springMVC_2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ springboot_practice_springMVC_2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,27 +1,16 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class)    //수정 요구사항 추가
     private Long id;
 
-    @NotBlank(groups = {UpdateCheck.class, SaveCheck.class})
     private String itemName;
 
-    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
-    @Range(min = 1_000, max = 1_000_000, groups = {UpdateCheck.class, SaveCheck.class})
     private Integer price;
 
-    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class})    //수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
