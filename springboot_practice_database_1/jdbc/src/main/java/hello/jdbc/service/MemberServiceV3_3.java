9fdd387d8b14229b19ce565124d98fd523128111
LDY <leedy3838@naver.com>
2023-12-31 13:09:44 +0900

    트랜잭션 문제 해결 – 트랜잭션 AOP 이해

--- springboot_practice_database_1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ springboot_practice_database_1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -4,34 +4,20 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 
 @Slf4j
 @RequiredArgsConstructor
-public class MemberServiceV3_2 {
+public class MemberServiceV3_3 {
 
-//    private final PlatformTransactionManager transactionManager;
-    private final TransactionTemplate txTemplate;
+
     private final MemberRepositoryV3 memberRepository;
 
-    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
-        this.txTemplate = new TransactionTemplate(transactionManager);
-        this.memberRepository = memberRepository;
-    }
-
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-
-        txTemplate.executeWithoutResult((status) -> {
-            //비즈니스 로직
-            try {
-                bizLogic(fromId, toId, money);
-            } catch (SQLException e) {
-                throw new IllegalStateException(e);
-            }
-        });
+        bizLogic(fromId, toId, money);
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
