7d9c0da74bf0632ca6a4463afbe8eb5da55b8881
LDY <leedy3838@naver.com>
2024-01-08 14:12:13 +0900

    JPA 적용1 - 개발

--- springboot_practice_database_2/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ springboot_practice_database_2/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -1,12 +1,19 @@
 package hello.itemservice.domain;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
+@Entity
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
