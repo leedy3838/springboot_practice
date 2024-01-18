faacb530683435b1274fbb606c2ce6a64029e5b1
LDY <leedy3838@naver.com>
2024-01-18 11:57:54 +0900

    스프링 트랜잭션 전파 활용2 - 커밋, 롤백

--- springboot_practice_database_2/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springboot_practice_database_2/springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -29,8 +30,26 @@ class MemberServiceTest {
         //when
         memberService.joinV1(username);
 
-        //when : 모든 데이터가 정상 저장된다.
+        //then : 모든 데이터가 정상 저장된다.
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * MemberService        @Transactional:OFF
+     * MemberRepository     @Transactional:ON
+     * LogRepository        @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_success";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //then : 예외가 발생되어 member만 저장
+        assertTrue(memberRepository.find(username).isPresent());
     }
 }
