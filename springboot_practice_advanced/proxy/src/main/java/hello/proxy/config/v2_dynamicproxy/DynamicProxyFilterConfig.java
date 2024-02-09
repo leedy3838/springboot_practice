e6d499677c00f43a4deadbf6b15d22967d6c4047
LDY <leedy3838@naver.com>
2024-02-09 17:37:39 +0900

    프록시 팩토리 - 적용2

--- springboot_practice_advanced/proxy/src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyFilterConfig.java
+++ springboot_practice_advanced/proxy/src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyFilterConfig.java
@@ -1,7 +1,6 @@
 package hello.proxy.config.v2_dynamicproxy;
 
 import hello.proxy.app.v1.*;
-import hello.proxy.config.v2_dynamicproxy.handler.LogTraceBasicHandler;
 import hello.proxy.config.v2_dynamicproxy.handler.LogTraceFilterHandler;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.context.annotation.Bean;
@@ -16,22 +15,22 @@ public class DynamicProxyFilterConfig {
 
     @Bean
     public OrderControllerV1 orderControllerV1(LogTrace logTrace) {
-        OrderControllerV1 orderControllerV1 = new OrderControllerV1Impl(orderServiceV1(logTrace));
+        OrderControllerV1 orderControllerV2 = new OrderControllerV1Impl(orderServiceV1(logTrace));
 
         OrderControllerV1 proxy = (OrderControllerV1) Proxy.newProxyInstance(OrderControllerV1.class.getClassLoader(),
                 new Class[]{OrderControllerV1.class},
-                new LogTraceFilterHandler(orderControllerV1, logTrace, PATTERNS));
+                new LogTraceFilterHandler(orderControllerV2, logTrace, PATTERNS));
 
         return proxy;
     }
 
     @Bean
     public OrderServiceV1 orderServiceV1(LogTrace logTrace) {
-        OrderServiceV1 orderServiceV1 = new OrderServiceV1Impl(orderRepositoryV1(logTrace));
+        OrderServiceV1 orderServiceV2 = new OrderServiceV1Impl(orderRepositoryV1(logTrace));
 
         OrderServiceV1 proxy = (OrderServiceV1) Proxy.newProxyInstance(OrderServiceV1.class.getClassLoader(),
                 new Class[]{OrderServiceV1.class},
-                new LogTraceFilterHandler(orderServiceV1, logTrace, PATTERNS));
+                new LogTraceFilterHandler(orderServiceV2, logTrace, PATTERNS));
 
         return proxy;
     }
