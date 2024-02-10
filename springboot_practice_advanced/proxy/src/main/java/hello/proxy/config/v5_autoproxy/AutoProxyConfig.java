9027d40cdc90ffd578b01c37490683b5a126d943
LDY <leedy3838@naver.com>
2024-02-10 11:42:43 +0900

    스프링이 제공하는 빈 후처리기1

---
+++ springboot_practice_advanced/proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -0,0 +1,22 @@
+package hello.proxy.config.v5_autoproxy;
+
+import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AutoProxyConfig {
+
+    public Advisor Advisor1(LogTrace logTrace) {
+        //pointcut
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedNames("request*", "order*", "save*");
+        //advice
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+}
