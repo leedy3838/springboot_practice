fb0628b959cec495e4293112bf51152d074f04de
LDY <leedy3838@naver.com>
2024-02-18 12:20:52 +0900

    단방향 연관관계

--- springbooot_practice_jpa_basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ springbooot_practice_jpa_basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,13 +15,18 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
             Member member = new Member();
-            member.setUsername("A");
-            member.setRoleType(RoleType.USER);
+            member.setTeam(team);
             em.persist(member);
 
-            System.out.println("======================");
+            Member findMember = em.find(Member.class, member.getId());
+
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam);
 
             tx.commit();
         } catch (Exception e) {
--- springbooot_practice_jpa_basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ springbooot_practice_jpa_basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -8,29 +8,19 @@ import java.util.Date;
 @Entity
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "username")
     private String username;
 
-    private Integer age;
+//    @Column(name = "team_id")
+//    private Long teamId;
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
-
-    public Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -48,43 +38,11 @@ public class Member {
         this.username = username;
     }
 
-    public Integer getAge() {
-        return age;
+    public Team getTeam() {
+        return team;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
