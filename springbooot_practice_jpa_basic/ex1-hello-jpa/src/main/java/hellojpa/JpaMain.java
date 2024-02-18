b24aa1d3d915679d777d4f33de199e5779ea801f
LDY <leedy3838@naver.com>
2024-02-18 15:43:02 +0900

    양방향 연관관계와 연관관계의 주인 1- 기본

--- springbooot_practice_jpa_basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ springbooot_practice_jpa_basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,13 +20,19 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
+            member.setUsername("member1");
             member.setTeam(team);
             em.persist(member);
 
-            Member findMember = em.find(Member.class, member.getId());
+            em.flush();
+            em.clear();
 
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam);
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e) {
